7ebaae1f908745afa8fe85f375e0b2623eb280ca
Kristen Myers <kristenmyers1118@gmail.com>
2026-09-05 13:29:57 +0100

    Success URL.js
    Updated success_url to https://www.yourcozygamer.com/test-success

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/create-checkout-session', async (req, res) => {
       },
     ],
     mode: 'subscription',
-    success_url: `${YOUR_DOMAIN}?success=true`,
+    success_url: `https://www.yourcozygamer.com/test-success`,
     // Provide a name (for example, hosted_web_0001) to label this Checkout integration and measure its conversion independently
     integration_identifier: '{{INTEGRATION_ID}}',
   });
